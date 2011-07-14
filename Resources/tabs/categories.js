f9e890eff75d3ce96f0e8b1c3bd23e04f2d2b0ab
Demetrio Cruz <hesenrre@gmail.com>
2011-07-13 21:07:42 -0500

    top aligment

--- Resources/tabs/categories.js
+++ Resources/tabs/categories.js
@@ -4,7 +4,7 @@ var win = Titanium.UI.currentWindow;
 var categoriesTable = Titanium.UI.createTableView({
 	data:[{title:'Stars'},{title:'Swiming Pool'},{title:'No Swiming Pool'},{title:'Parking'},{title:'No parking'}],
 	scrollable:true,
-	top:30
+	top:0
 });
 
 win.add(categoriesTable);
